e4b30e0f3ac9b1af99be6afac5fb4d2978030005
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-01-14 01:19:28 +0100

    Add more times to notes, add comparison to test

--- compilerun.sh
+++ compilerun.sh
@@ -2,3 +2,5 @@
 
 gcc -O0 -o cavdet cav_det_init.c
 echo `./cavdet; ./cavdet; ./cavdet; ./cavdet; ./cavdet` | grep -oh '\w[0-9]\+\.[0-9]\+\w' | awk ' { sum += $1; print $1 } END { print "Average: " sum/NR }'
+./cmp
+
