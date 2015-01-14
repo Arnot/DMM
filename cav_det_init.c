f44c032861630e79c59a696769870a3f90ea46db
Arno Tiemersma <arno.tiemersma@gmail.com>
2015-01-14 16:22:41 +0100

    Fix up for-loop ranges in GaussBlur

--- cav_det_init.c
+++ cav_det_init.c
@@ -172,8 +172,8 @@ void GaussBlur (int image_in[N][M], int image_out[N][M])
 	int tot=0;
 	for (k=-GB; k<=GB; ++k) tot+=Gauss[abs(k)];
 
-	for (x=GB; x<=N-1-GB; ++x)
-		for (y=GB; y<=M-1-GB; ++y) {
+	for (x=0; x<=N; ++x)
+		for (y=0; y<=M; ++y) {
 			if (x < GB || x > N-1-GB || y < GB || y > N-1-GB) {
 				tmp[x][y] = 0;
 			} else {
@@ -184,8 +184,8 @@ void GaussBlur (int image_in[N][M], int image_out[N][M])
 			}
 		}
 
-	for (x=GB; x<=N-1-GB; ++x)
-		for (y=GB; y<=M-1-GB; ++y) {
+	for (x=0; x<=N; ++x)
+		for (y=0; y<=M; ++y) {
 			if (x < GB || x > N-1-GB || y < GB || y > N-1-GB) {
 				image_out[x][y] = 0;
 			} else {
